f8c833d6547b6ac2a6c4e320777597b1a55482d4
Fabiola Spredemann <fabispredemann@gmail.com>
2022-08-19 09:09:29 -0300

    Weather Observation Station 14

---
+++ SQL/aggregation_station14.sql
@@ -0,0 +1,8 @@
+/*
+Query the greatest value of the Northern Latitudes (LAT_N) from STATION that is less than 137.2345. Truncate your answer to 4 decimal places
+
+*/
+
+select round(max(LAT_N), 4) as lat
+from station
+where LAT_N < '137.2345'
